3be1233c113291d3b1e5f773c18e15bcb1f13bc3
gauravjagwani <gauravjagwani8@gmail.com>
2024-10-08 02:10:50 +0530

    added POST, GET, DELETE and PATCH methods for app

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const connectDB = require("./config/database");
 app.use(express.json());
 const User = require("./models/user");
 
+//* POST API for signup
 app.post("/signup", (req, res) => {
   console.log(req.body);
 
@@ -25,6 +26,58 @@ app.post("/signup", (req, res) => {
     .catch((err) => {
       res.status(400).send("Error saving the User:" + err.message);
     });
+});
+
+// * GET user API
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const user = await User.find({
+      emailId: userEmail,
+    });
+    if (user.length === 0) {
+      res.status(401).send("User not found");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+// * Feed API - GET / feed- get all the users from the database
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+//* Delete API
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    res.send("User deleted Successfully");
+  } catch (err) {
+    res.status(404).send("Something went Wrong");
+  }
+});
+
+//* Update API
+
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    const user = await User.findByIdAndUpdate({ _id: userId }, data);
+    res.send("User Updated Successfully");
+  } catch (err) {
+    res.status(404).send("Something went Wrong");
+  }
 });
 
 connectDB()
