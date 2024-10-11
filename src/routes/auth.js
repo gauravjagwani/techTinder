61158e819f4d40e2d6a0dfd49b535cdfb5ea3a47
gauravjagwani <gauravjagwani8@gmail.com>
2024-10-11 18:22:42 +0530

    created different routers

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -71,4 +71,15 @@ authRouter.post("/login", async (req, res) => {
   }
 });
 
+// * Logout API
+authRouter.post("/logout", async (req, res) => {
+  try {
+    res
+      .cookie("token", null, { expires: new Date(Date.now()) })
+      .send("User Logged out Successfully");
+  } catch (err) {
+    res.status(400).send("Error in logging out");
+  }
+});
+
 module.exports = authRouter;
--- src/routes/profile.js
+++ src/routes/profile.js
@@ -5,11 +5,13 @@ const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
 const { userAuth } = require("../middlewares/auth");
 const User = require("../models/user");
+const { validateEditProfileData } = require("../utils/validation");
 
 const { validateSignUpData } = require("../utils/validation");
 const profileRouter = express.Router();
 
-profileRouter.get("/profile", userAuth, async (req, res) => {
+//* GET Profile
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
   try {
     const user = req.user;
     console.log("Profile loggin success");
@@ -18,4 +20,30 @@ profileRouter.get("/profile", userAuth, async (req, res) => {
     res.status(400).send("ERROR: " + err.message);
   }
 });
+
+//* PATCH Profile
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+  try {
+    if (!validateEditProfileData(req)) {
+      throw new Error("Invalid Edit Request");
+    }
+    const isURLValid = validator.isURL(req.body.photoUrl);
+    if (!isURLValid) {
+      throw new Error("Invalid Photo URL");
+    }
+
+    const LoggedInUser = req.user;
+    if (!LoggedInUser) {
+      throw new Error("Invalid Edit Request");
+    }
+
+    Object.keys(req.body).forEach((key) => (LoggedInUser[key] = req.body[key]));
+    await LoggedInUser.save();
+
+    res.send(`${LoggedInUser.firstName} Data Updated Successfully`);
+  } catch (err) {
+    res.status(400).send("ERROR :" + err.message);
+  }
+});
+
 module.exports = profileRouter;
