5e41ab89467c59b33c232f90e8b5018045a11b0a
gauravjagwani <gauravjagwani8@gmail.com>
2024-10-15 11:21:03 +0530

    added feed api and pagination feat

--- src/app.js
+++ src/app.js
@@ -41,14 +41,14 @@ app.get("/user", async (req, res) => {
 });
 
 // * Feed API - GET / feed- get all the users from the database
-app.get("/feed", async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.send(users);
-  } catch (err) {
-    res.status(400).send("Something went wrong");
-  }
-});
+// app.get("/feed", async (req, res) => {
+//   try {
+//     const users = await User.find({});
+//     res.send(users);
+//   } catch (err) {
+//     res.status(400).send("Something went wrong");
+//   }
+// });
 
 //* Delete API
 
--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const authRouter = require("./auth");
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequestModel = require("../models/connectionRequest");
+const User = require("../models/user");
 const userRouter = express.Router();
 
 // Get all the Pending Connection Request for LoggedIn User
@@ -30,6 +31,7 @@ userRouter.get("/user/requests/recieved", userAuth, async (req, res) => {
   }
 });
 
+// Get Connections which got accepted by other users
 userRouter.get("/user/connections", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
@@ -67,6 +69,51 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
     res.json({ data: data });
   } catch (err) {
     res.status(400).send({ message: err.message });
+  }
+});
+
+// Get Feed
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+  try {
+    // User should see all user cards except
+    //* his own card
+    //* his connections
+    // * ignored people
+    // * already sent the connection request (already swipped right(interested))
+
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit;
+    const skip = (page - 1) * limit;
+
+    const loggedInUser = req.user;
+    const { _id } = loggedInUser;
+    const connectionRequests = await ConnectionRequestModel.find({
+      $or: [{ fromUserId: _id }, { toUserId: _id }],
+    }).select("fromUserId toUserId");
+
+    const hideUsersFromFeed = new Set();
+    connectionRequests.forEach((req) => {
+      hideUsersFromFeed.add(req.fromUserId.toString());
+      hideUsersFromFeed.add(req.toUserId.toString());
+    });
+
+    const users = await User.find({
+      $and: [
+        {
+          _id: { $nin: Array.from(hideUsersFromFeed) },
+        },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    })
+      .select(["firstName", "lastName", "photoUrl", "about", "skills", "age"])
+      .skip(skip)
+      .limit(limit);
+
+    res.json({ data: users });
+  } catch (err) {
+    res.send();
   }
 });
 module.exports = userRouter;
